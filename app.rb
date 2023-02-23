f59dda9a1029d72566075e4d38847482d1143407
Joel Cornelius <tubsandcans@me.com>
2023-02-23 17:14:44 -0600

    Give delete button red color.

--- app.rb
+++ app.rb
@@ -36,6 +36,7 @@ __END__
     %meta{name: "viewport", content: "width=device-width", "initial-scale": 1.0}
     %link{rel: "stylesheet", href: "https://cdn.simplecss.org/simple.min.css"}
     %link{rel: "stylesheet", href: "https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css"}
+    %link{rel: "stylesheet", href: "main.css"}
     %script{src: "https://cdn.jsdelivr.net/npm/flatpickr"}
     %title Turn and Burn
   %body{onload: "flatpickr('.cycle-start', {enableTime: true});"}
