168d02f5be98a5fcb865cf40275018483754665e
Joel Cornelius <tubsandcans@me.com>
2023-03-08 17:06:27 -0600

    Non-null value create-column migrations a no-no.
    Could do this with multiple migrations, but less noisy this way.
    No other environments running this code I think it's safe :)

--- db/migrations/1_create_pumps.rb
+++ db/migrations/1_create_pumps.rb
@@ -3,6 +3,7 @@ Sequel.migration do
     create_table :pumps do
       primary_key :id
       Integer :pin_number, unique: true, null: false
+      Integer :rate, null: false
       DateTime :cycle_start, null: false
     end
   end
--- db/migrations/2_add_rate_to_pumps.rb
+++
@@ -1,5 +0,0 @@
-Sequel.migration do
-  change do
-    add_column :pumps, :rate, Integer, null: false
-  end
-end
