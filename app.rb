5c5142a2d368dab0a507527a60a41804971eb933
Joel Cornelius <tubsandcans@me.com>
2023-04-19 13:39:32 -0500

    Remove useless variable from calibrate action body.

--- app.rb
+++ app.rb
@@ -39,13 +39,12 @@ module OptimalRain
 
     get "/:id/calibrate" do
       pump = Pump.first(id: params[:id])
-      gpio_pin = OptimalRain::PUMP[:pins][pump.pin_number]
-      gpio_pin.set_value(HIGH)
+      OptimalRain::PUMP[:pins][pump.pin_number].set_value(HIGH)
       calibration = Rufus::Scheduler.new
       OptimalRain::PUMP_CALIBRATIONS << pump.pin_number
       calibration.in("#{OptimalRain::CALIBRATION_DURATION}s") do
         OptimalRain::PUMP_CALIBRATIONS.delete(pump.pin_number)
-        gpio_pin.set_value(LOW)
+        OptimalRain::PUMP[:pins][pump.pin_number].set_value(LOW)
       end
       redirect to("/")
     end
